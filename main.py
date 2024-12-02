082b1e421aac61b00d420a41551c0d4e1a8dd1c3
jotap1101 <jotap1101.notebook@gmail.com>
2024-12-02 11:16:21 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+
+from flask import Flask, jsonify, request, send_file, send_from_directory
+from langchain_core.messages import HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import json
+import os
+
+# Load environment variables
+load_dotenv()
+
+# Google API Key
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+
+# Create the Flask app
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_file('web/index.html')
+
+@app.route('/api/generate', methods=['POST'])
+def generate_api():
+    if request.method == 'POST':
+        if GOOGLE_API_KEY == '':
+            return jsonify({ 'error': '''
+                To get started, get an API key at
+                https://g.co/ai/idxGetGeminiKey and enter it in
+                main.py
+                '''.replace('\n', '') })
+        try:
+            req_body = request.get_json()
+            content = req_body.get('contents')
+            language = req_body.get('language', 'pt-br')  # Default to English if not provided
+            model = ChatGoogleGenerativeAI(model=req_body.get('model'))
+            message = HumanMessage(
+                content=content,
+                language=language  # Set the language for the response
+            )
+            response = model.stream([message])
+            def stream():
+                for chunk in response:
+                    yield 'data: %s\n\n' % json.dumps({ 'text': chunk.content })
+
+            return stream(), {'Content-Type': 'text/event-stream'}
+
+        except Exception as e:
+            return jsonify({ 'error': str(e) })
+
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory('web', path)
+
+if __name__ == '__main__':
+    app.run(port=int(os.environ.get('PORT', 80)))
